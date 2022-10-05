461ed65fb1c8d88e556be6d651d4131ece5ebbde
sheilagarcia121 <113448445+sheilagarcia1218@users.noreply.github.com>
2022-10-05 13:59:34 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+public static void main(String[] args) {
+int count = 1;
+System.out.println("Printing Numbers from 1 to 10");
+while (count <= 10) {
+System.out.println(count++);
+}
+}
+}
